9abf2f1fab68cc7a9d767dc0cf3a2896d956550b
Song Xiaoguang <xgsong@mobvoi.com>
2016-10-24 10:50:54 +0800

    ShareSDKDemo

--- SharSDKDemo3/AppDelegate.m
+++ SharSDKDemo3/AppDelegate.m
@@ -8,44 +8,104 @@
 
 #import "AppDelegate.h"
 
+#import <ShareSDK/ShareSDK.h>
+#import <ShareSDKConnector/ShareSDKConnector.h>
+
+#import <TencentOpenAPI/QQApiInterface.h>
+#import <TencentOpenAPI/TencentOAuth.h>
+
+#import "WXApi.h"
+
+#import "WeiboSDK.h"
+
 @interface AppDelegate ()
 
 @end
 
 @implementation AppDelegate
 
+- (BOOL)application:(UIApplication *)application
+    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  // Override point for customization after application launch.
+  [ShareSDK registerApp:@"182947e7afac0"
+      activePlatforms:@[
+        @(SSDKPlatformTypeSinaWeibo),
+        @(SSDKPlatformSubTypeWechatSession),
+        @(SSDKPlatformSubTypeWechatTimeline),
+        @(SSDKPlatformTypeQQ)
+      ]
+      onImport:^(SSDKPlatformType platformType) {
+        switch (platformType) {
+        case SSDKPlatformTypeSinaWeibo:
+          [ShareSDKConnector connectWeChat:[WeiboSDK class]];
+          break;
+        case SSDKPlatformTypeWechat:
+          [ShareSDKConnector connectWeChat:[WXApi class]];
+          break;
+        case SSDKPlatformTypeQQ:
+          [ShareSDKConnector connectQQ:[QQApiInterface class]
+                     tencentOAuthClass:[TencentOAuth class]];
+          break;
+        default:
+          break;
+        }
+      }
+      onConfiguration:^(SSDKPlatformType platformType,
+                        NSMutableDictionary *appInfo) {
+        switch (platformType) {
+        case SSDKPlatformTypeSinaWeibo:
+          [appInfo
+              SSDKSetupSinaWeiboByAppKey:@"3144028685"
+                               appSecret:@"11abfe456bc8eefbab49fe7bbcd90bf0"
+                             redirectUri:@"http://www.sharesdk.cn"
+                                authType:SSDKAuthTypeBoth];
+          break;
+        case SSDKPlatformTypeWechat:
+          [appInfo SSDKSetupWeChatByAppId:@"wx030db67392eff055"
+                                appSecret:@"e06c7162a5ec371886f1be9ae21d4d82"];
+          break;
+        case SSDKPlatformTypeQQ:
+          [appInfo SSDKSetupQQByAppId:@"" appKey:@"" authType:SSDKAuthTypeBoth];
+          break;
+        default:
+          break;
+        }
+      }];
   return YES;
 }
 
-
 - (void)applicationWillResignActive:(UIApplication *)application {
-  // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-  // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+  // Sent when the application is about to move from active to inactive state.
+  // This can occur for certain types of temporary interruptions (such as an
+  // incoming phone call or SMS message) or when the user quits the application
+  // and it begins the transition to the background state.
+  // Use this method to pause ongoing tasks, disable timers, and invalidate
+  // graphics rendering callbacks. Games should use this method to pause the
+  // game.
 }
-
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
-  // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-  // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+  // Use this method to release shared resources, save user data, invalidate
+  // timers, and store enough application state information to restore your
+  // application to its current state in case it is terminated later.
+  // If your application supports background execution, this method is called
+  // instead of applicationWillTerminate: when the user quits.
 }
-
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
-  // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+  // Called as part of the transition from the background to the active state;
+  // here you can undo many of the changes made on entering the background.
 }
-
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
-  // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+  // Restart any tasks that were paused (or not yet started) while the
+  // application was inactive. If the application was previously in the
+  // background, optionally refresh the user interface.
 }
-
 
 - (void)applicationWillTerminate:(UIApplication *)application {
-  // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+  // Called when the application is about to terminate. Save data if
+  // appropriate. See also applicationDidEnterBackground:.
 }
-
 
 @end
--- SharSDKDemo3/ViewController.m
+++ SharSDKDemo3/ViewController.m
@@ -8,7 +8,26 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+//  系统
+#import <ShareSDK/ShareSDK.h>
+#import <ShareSDKUI/ShareSDK+SSUI.h>
+//  自定义
+#import <ShareSDKUI/SSUIEditorViewStyle.h>
+#import <ShareSDKUI/SSUIShareActionSheetCustomItem.h>
+#import <ShareSDKUI/SSUIShareActionSheetStyle.h>
+
+@interface ViewController () <UIImagePickerControllerDelegate,
+                              UINavigationControllerDelegate>
+
+@property(nonatomic, strong) UIButton *btn;
+
+@property(nonatomic, strong) UIButton *shareBtn;
+
+@property(nonatomic, strong) UIImagePickerController *picker;
+
+@property(nonatomic, strong) UIImage *image;
+
+@property(nonatomic, strong) UIImageView *imageView;
 
 @end
 
@@ -16,14 +35,136 @@
 
 - (void)viewDidLoad {
   [super viewDidLoad];
-  // Do any additional setup after loading the view, typically from a nib.
+
+  self.btn = [UIButton buttonWithType:UIButtonTypeCustom];
+  self.btn.frame =
+      CGRectMake(self.view.bounds.size.width / 2 - 50, 100, 100, 20);
+  [self.btn setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+  [self.btn setTitle:@"Image" forState:UIControlStateNormal];
+  [self.btn addTarget:self
+                action:@selector(btnAction)
+      forControlEvents:UIControlEventTouchUpInside];
+  [self.view addSubview:self.btn];
+
+  self.shareBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+  self.shareBtn.frame =
+      CGRectMake(self.view.bounds.size.width / 2 - 50, 150, 100, 20);
+  [self.shareBtn setTitleColor:[UIColor redColor]
+                      forState:UIControlStateNormal];
+  [self.shareBtn setTitle:@"Share" forState:UIControlStateNormal];
+  [self.shareBtn addTarget:self
+                    action:@selector(shareAction)
+          forControlEvents:UIControlEventTouchUpInside];
+  [self.view addSubview:self.shareBtn];
+
+  self.imageView = [[UIImageView alloc]
+      initWithFrame:CGRectMake(0, 200, self.view.bounds.size.width,
+                               self.view.bounds.size.width)];
+  [self.view addSubview:self.imageView];
+
+  self.picker = [[UIImagePickerController alloc] init];
+  self.picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+  self.picker.delegate = self;
+  self.picker.allowsEditing = YES;
+
+  self.image = [UIImage imageNamed:@"junxi4.jpeg"];
 }
 
+- (void)imagePickerController:(UIImagePickerController *)picker
+didFinishPickingMediaWithInfo:(NSDictionary<NSString *, id> *)info {
+  self.image = [info objectForKey:UIImagePickerControllerOriginalImage];
+  self.imageView.image = self.image;
+  [self dismissViewControllerAnimated:picker completion:nil];
+}
+
+- (void)shareAction {
+
+  //  设置分享参数
+  NSMutableDictionary *shareParams = [NSMutableDictionary dictionary];
+  [shareParams SSDKSetupShareParamsByText:@"分享"
+                                   images:self.image
+                                      url:nil
+                                    title:@"Ticwatch运动分享"
+                                     type:SSDKContentTypeAuto];
+  //  设置分享sheet样式
+  //  分享菜单背景颜色
+  //  [SSUIShareActionSheetStyle setActionSheetBackgroundColor:[UIColor
+  //  redColor]];
+  //  分享菜单颜色
+  //  [SSUIShareActionSheetStyle setActionSheetColor:[UIColor greenColor]];
+  //  分享菜单社交平台文本颜色
+  //  [SSUIShareActionSheetStyle setItemNameColor:[UIColor blueColor]];
+  //  分享菜单社交平台文本字体，防止显示不全问题
+  [SSUIShareActionSheetStyle setItemNameFont:[UIFont systemFontOfSize:10]];
+  //  分享菜单样式
+  //  [SSUIShareActionSheetStyle
+  //      setShareActionSheetStyle:ShareActionSheetStyleSimple];
+  //  分享编辑界面导航栏颜色
+  //  [SSUIEditorViewStyle setiPadNavigationBarBackgroundColor:[UIColor
+  //  redColor]];
+  //  分享编辑界面标题颜色
+  //  [SSUIEditorViewStyle setTitleColor:[UIColor greenColor]];
+  //  分享编辑界面取消颜色
+  //  [SSUIEditorViewStyle setCancelButtonLabelColor:[UIColor blueColor]];
+  //  [SSUIEditorViewStyle setShareButtonLabelColor:[UIColor blueColor]];
+  //  分享编辑界面状态栏风格
+  //  [SSUIEditorViewStyle setStatusBarStyle:UIStatusBarStyleLightContent];
+
+  SSUIShareActionSheetCustomItem *itemWeibo = [SSUIShareActionSheetCustomItem
+      itemWithPlatformType:SSDKPlatformTypeSinaWeibo];
+  itemWeibo.label = @"微博";
+
+  SSUIShareActionSheetCustomItem *itemWechatSession =
+      [SSUIShareActionSheetCustomItem
+          itemWithPlatformType:SSDKPlatformSubTypeWechatSession];
+  itemWechatSession.label = @"微信";
+
+  SSUIShareActionSheetCustomItem *itemWechatTimeLine =
+      [SSUIShareActionSheetCustomItem
+          itemWithPlatformType:SSDKPlatformSubTypeWechatTimeline];
+  itemWechatTimeLine.label = @"朋友圈";
+
+  SSUIShareActionSheetCustomItem *itemQQ =
+      [SSUIShareActionSheetCustomItem itemWithPlatformType:SSDKPlatformTypeQQ];
+  itemQQ.label = @"QQ";
+
+  //  分享
+  [ShareSDK showShareActionSheet:nil
+                           items:@[
+                             @(SSDKPlatformTypeSinaWeibo),
+                             @(SSDKPlatformSubTypeWechatTimeline),
+                             @(SSDKPlatformSubTypeWechatSession)
+                           ]
+                     shareParams:shareParams
+             onShareStateChanged:^(
+                 SSDKResponseState state, SSDKPlatformType platformType,
+                 NSDictionary *userData, SSDKContentEntity *contentEntity,
+                 NSError *error, BOOL end) {
+               switch (state) {
+               case SSDKResponseStateSuccess:
+                 NSLog(@"成功");
+                 break;
+               case SSDKResponseStateFail:
+                 NSLog(@"失败");
+                 break;
+               default:
+                 break;
+               }
+             }];
+}
+
+- (void)btnAction {
+
+  if ([UIImagePickerController
+          isSourceTypeAvailable:
+              UIImagePickerControllerSourceTypePhotoLibrary]) {
+    [self presentViewController:self.picker animated:YES completion:nil];
+  }
+}
 
 - (void)didReceiveMemoryWarning {
   [super didReceiveMemoryWarning];
   // Dispose of any resources that can be recreated.
 }
-
 
 @end
